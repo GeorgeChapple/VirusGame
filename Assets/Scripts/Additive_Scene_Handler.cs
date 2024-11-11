b9f989488e58a4135b3b160e57e299df9a5ac7c0
Jason L <146436532+JTRMreal@users.noreply.github.com>
2024-11-11 18:32:28 +0000

    windows sorta there
    used the i drag handler to allow me to drag ui around, using a camera that renders to a render texture turned into a material so i could put it on a ui image component.
    references - https://www.youtube.com/watch?v=CuAEp4x2vnQ
    https://discussions.unity.com/t/show-camera-view-in-ui/906399/2

--- Assets/Scripts/Additive_Scene_Handler.cs
+++ Assets/Scripts/Additive_Scene_Handler.cs
@@ -6,9 +6,10 @@ using UnityEngine.UI;
 
 public class Additive_Scene_Handler : MonoBehaviour
 {
+    [SerializeField] private Canvas canvas;
     public void buttonPress()
     {
-        print("trying to add scene");
         SceneManager.LoadScene(1,LoadSceneMode.Additive);
+        //canvas.enabled = false;
     }
 }
