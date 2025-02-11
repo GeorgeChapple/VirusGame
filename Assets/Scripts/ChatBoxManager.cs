c02378eee6545f7e61d5d1fae2eb15d83ec6096f
George <147159990+Hat-Wearing-Man@users.noreply.github.com>
2025-02-11 12:24:15 +0000

    Fixed chatbox speech bubble issues
    *Fixed chatbox content rect not scaling properly, rect now scales based on grid layout group cell size + spacing size.
    *Changed chatbox content rect grid layout group start/child alignments to upper left by default.
    -This makes the grid layout group add child objects starting from the top left and go down.
    *Fixed chatbox not setting the vertical normal position properly when a new speech bubble is spawned.

--- Assets/Scripts/ChatBoxManager.cs
+++ Assets/Scripts/ChatBoxManager.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class ChatBoxManager : MonoBehaviour
 {
     private SpeechManager speechManager; // Stores the speech manager script attatched to the chatbox
-    private UnityEngine.RectTransform contentRect; // Stores the content UI, this is where the speech bubbles get placed on to be scrolled through
+    private RectTransform contentRect; // Stores the content UI, this is where the speech bubbles get placed on to be scrolled through
+    private GridLayoutGroup contentRectGridBox;
 
     private void Awake() {
         contentRect = transform.Find("Viewport/Content").GetComponent<UnityEngine.RectTransform>();
+        contentRectGridBox = contentRect.GetComponent<GridLayoutGroup>();
         speechManager = GetComponent<SpeechManager>();
         speechManager.newBubbleCreated.AddListener(AddSpeechBubble);
     }
@@ -20,10 +22,9 @@ public class ChatBoxManager : MonoBehaviour
 
     // Listens for when speech manager spawns a new speech bubble, sets the default values and parent of the new bubble
     private void AddSpeechBubble() {
-        GetComponent<ScrollRect>().verticalNormalizedPosition = 0;
         GameObject bubble = speechManager.speechBubbles[speechManager.speechBubbles.Count - 1];
-        contentRect.sizeDelta += new Vector2(0, bubble.GetComponent<Image>().rectTransform.sizeDelta.y + 5);
+        contentRect.sizeDelta += new Vector2(0, contentRectGridBox.cellSize.y + contentRectGridBox.spacing.y);
         bubble.transform.parent = contentRect;
-        //bubble.transform.position = new Vector3(contentRect.anchorMax.x + 400, contentRect.anchorMax.y + 250, 0);
+        GetComponent<ScrollRect>().verticalNormalizedPosition = 0;
     }
 }
