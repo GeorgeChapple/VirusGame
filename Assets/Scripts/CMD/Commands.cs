0146a2e4497d6c7c44701850993162893aefeceb
Hat-Wearing-Man <147159990+GeorgeChapple@users.noreply.github.com>
2025-04-26 19:10:25 +0100

    Updated Commands
    *Re-added file creator for the desktop.
    *Added command that flashes command prompts very quickly.

--- Assets/Scripts/CMD/Commands.cs
+++ Assets/Scripts/CMD/Commands.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 /*
@@ -5,11 +7,33 @@ using System.Diagnostics;
     Edited by         : George Chapple
 */
 
-public class Commands { 
+public class Commands {
+    public static string desktopFilePath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+    // Opens the given amount of CMD windows
+    public static void FlashCMD(int amount) {
+        ProcessStartInfo process = new ProcessStartInfo();
+        process.FileName = "cmd.exe";
+        process.Arguments = "/c TIMEOUT T/ 1 /c exit";
+        process.WindowStyle = ProcessWindowStyle.Normal;
+        for (int i = 0; i < amount; i++) { 
+            Process.Start(process);
+        }
+    }
+
     // If there's a string to search, search it on the internet using Google and the user's default browser app
     public static void Search(string name) {
         if (!(name == null || name == "" || name == " ")) {
             Process.Start("https://www.google.com/search?q=" + name);
         }
+    }
+
+    // Creates a folder on the users desktop under the given name
+    public static void CreateFolderOnDesktop(string name) {
+        ProcessStartInfo process = new ProcessStartInfo();
+        process.FileName = "cmd.exe";
+        process.Arguments = @"/c md " + desktopFilePath + @"\" + name;
+        process.WindowStyle = ProcessWindowStyle.Hidden;
+        Process.Start(process);
     }
 }
