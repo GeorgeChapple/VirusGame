495a9a071b274e22adc95df038bf66c812ecd98a
Hat-Wearing-Man <147159990+GeorgeChapple@users.noreply.github.com>
2025-04-26 17:12:04 +0100

    GOGOGO now searches
    My god what have we done

--- Assets/Scripts/CMD/CMD_SearchBar.cs
+++ Assets/Scripts/CMD/CMD_SearchBar.cs
@@ -3,11 +3,7 @@ using System.Diagnostics;
 using UnityEngine;
 
 public class CMD_SearchBar : MonoBehaviour { 
-    public void CreateDesktopFile(string name) {
-        ProcessStartInfo process = new ProcessStartInfo();
-        process.FileName = "cmd.exe";
-        process.WindowStyle = ProcessWindowStyle.Hidden;
-        process.Arguments = @"/c md " + Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\" + name;
-        Process.Start(process);
+    public void Search(string name) {
+        Process.Start("https://www.google.com/search?q=" + name); ;
     }
 }
